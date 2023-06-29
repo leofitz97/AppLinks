36ad9e58efbe1a18f94c8e88a8d28a521ff108b6
leofitz <Liomslemueldavies50@gmail.com>
2023-06-29 23:25:36 +0100

    .

--- api/appsApiTest.js
+++ api/appsApiTest.js
@@ -1,60 +1,67 @@
-const AppServices = require('../services/AppServices');
-const auth = require('./middleware/auth');
-const { validateCreateApp, validateUpdate } = require('./middleware/appValidations');
+const AppServices = require('../../services/AppServices');
+const auth = require('../../api/middleware/auth');
+const { validateCreateApp, validateUpdate } = require('../../api/middleware/appValidations');
+
+
 
 
 module.exports = (app)=>{
 
     const services = new AppServices();
 
-    app.post('/apps/add', auth(), validateCreateApp(), async(req, res)=>{
+    app.post('/test/apps/add', auth(), validateCreateApp(), async(req, res)=>{
+        
         try {
             const { name, icon, url } = req.body;
-            const token = await services.createApp({name:name, icon:icon, url:url});
+            // const token = await services.createApp({name:name, icon:icon, url:url});
             res.status(200).json({message:'app link has been created successfully!'});
-        }catch(err){ 
+        }catch(err){
             res.status(500).json({message:'an error occured creating app link!', error:'internal server error!'})
         }
     })
 
-    app.get('/apps/:id', auth(), async(req, res)=>{
+    app.get('/test/apps/:id', auth(), async(req, res)=>{
         try {
             const { id } = req.params;
-            const data = await services.editApp(id);
-            res.status(200).json({message:'success', data:data})
+            // const data = await services.editApp(id);
+            // res.status(200).json({message:'success', data:data})
+            res.status(200).json({message:'success'})
         }catch(err){
             res.status(500).json({message:'unable to fetch app data!', error:'internal server error!'})
         }
     })
 
-    app.patch('/apps/:id', auth(), validateUpdate(), async(req, res)=>{
+    app.patch('/test/apps/:id', auth(), validateUpdate(), async(req, res)=>{
         try {
             const { id } = req.params;
             const { name } = req.body;
-            await services.updateAppInfo({id:id, name:name});
+            // await services.updateAppInfo({id:id, name:name});
             res.status(200).json({message:'app has been successfully updated!'})
         }catch(err){
             res.status(500).json({message:'unable to update app data!', error:'internal server error!'})
         }
     })
 
-    app.delete('/apps/:id', auth(), async(req, res)=>{
+    app.delete('/test/apps/:id', auth(), async(req, res)=>{
         try {
             const { id } = req.params;
-            await services.deleteApp(id);
-            res.status(200).json({message:'app has been deleted successfully'})
+            // await services.deleteApp(id);
+            res.status(200).json({message:'app has been deleted successfully!'})
         }catch(err){
             res.status(500).json({message:'an error occured deleting the app!', error:'internal server error!'})
         }
     })
 
-    app.get('/apps', async(req, res)=>{
+    app.get('/test/apps', async(req, res)=>{
         try {
-            const data = await services.viewAllApps();
-            res.status(200).json({message:'success', data:data})
+            // const data = await services.viewAllApps();
+            // res.status(200).json({message:'success', data:data})
+            res.status(200).json({message:'success'})
         }catch(err){ console.log(err)
             res.status(500).json({message:'an error occured fetching apps', error:'internal server error!'})
         }
     })
+
+    return app;
 }
 
--- express-app.js
+++ express-app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { userApis, appApis } = require('./api');
+// const { userApis, appApis } = require('./api');
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-userApis(app);
-appApis(app);
-
-
 
 
 
