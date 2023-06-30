72a88bcf80ab43f51520b92b52196cf79a042848
leofitz <Liomslemueldavies50@gmail.com>
2023-06-30 14:20:10 +0100

    .

---
+++ config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config(); // this is important!
+
+
+module.exports = {
+  "test": {
+    "username": process.env.DB_USERNAME_TST,
+    "password": null,
+    "database": process.env.DB_DATABASE_TST,
+    "host":     process.env.DB_HOST_TST,
+    "dialect": "mysql"
+  },
+}
+
