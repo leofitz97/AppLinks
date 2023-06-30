5eafc4c45a10208b8bfa0df094263cb0a0fcedf4
leofitz <Liomslemueldavies50@gmail.com>
2023-06-30 16:16:19 +0100

    .

--- express-app.js
+++ express-app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 const { userApis, appApis } = require('./api');
 
-
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
